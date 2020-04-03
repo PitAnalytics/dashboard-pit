ebe22c7700a4716cb10cda1e2313bdd27b2fc10e
BernardoColorado <bernardocoloradodubois.hw@gmail.com>
2020-04-02 22:57:51 -0600

    ajustando proyecto

--- app/src/Controllers/Api/CovidController.php
+++ app/src/Controllers/Api/CovidController.php
@@ -15,7 +15,7 @@ class CovidController extends AbstractController{
 
     $this->container=$container;
     $this->setMainInstances();
-    $this->warehouseManager=$this->container['warehouse-manager'](['projectId'=>'estado-de-resultados']);
+    $this->warehouseManager=$this->container['warehouse-manager'](['projectId'=>'estado-de-resultados266105']);
 
   }
   /*******************************/
