8ea73f34fe6dd09d3a98cd70ca47e4f7593473e0
BernardoColorado <bernardocoloradodubois.hw@gmail.com>
2020-04-02 23:02:47 -0600

    corrigiendo queries de warehouse covid

--- app/src/Models/Warehouses/CovidWarehouse.php
+++ app/src/Models/Warehouses/CovidWarehouse.php
@@ -8,16 +8,17 @@ class CovidWarehouse extends AbstractWarehouse{
 
   public function index(){
 
-    $index = $this->bigQuery->query("SELECT
-    SUBSTR(CAST(date AS STRING),1,10) AS date,
-    SUM(confirmed) AS confirmed,
-    SUM(deaths) AS deaths,
-    SUM(recovered) AS recovered
-    FROM
-      `bigquery-public-data.covid19_jhu_csse.summary`
-    WHERE country_region = 'Mexico'
-    GROUP BY date
-    ORDER BY date ASC';");
+    $index = $this->bigQuery->query(
+    "SELECT 
+    SUBSTR(CAST(date AS STRING),1,10) AS date, 
+    SUM(confirmed) AS confirmed, 
+    SUM(deaths) AS deaths, 
+    SUM(recovered) AS recovered 
+    FROM 
+      `bigquery-public-data.covid19_jhu_csse.summary` 
+    WHERE country_region = 'Mexico' 
+    GROUP BY date 
+    ORDER BY date ASC; ");
 
     print_r($index);
 
